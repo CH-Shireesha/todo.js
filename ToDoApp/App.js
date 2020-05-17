a27ca4dd8e0fb44137c37e60bfc511fc1c5c53e1
CH-Shireesha <shirishach26@msitprogram.net>
2020-05-17 21:30:17 +0530

    ADD: ToDoList

--- ToDoApp/App.js
+++ ToDoApp/App.js
@@ -2,20 +2,44 @@ import * as React from 'react';
 import { Text, View, StyleSheet, FlatList } from 'react-native';
 import Constants from 'expo-constants';
 import Header from './components/header';
+import TodoItem from './components/ToDoItem';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 export default function App() {
+
+const [todos, setTodos] = React.useState([
+]);
+
+const pressHandler = (key) => {
+  setTodos(prevTodos => {
+    return prevTodos.filter(todo => todo.key != key);
+  });
+};
+
+const submitHandler = (text) => {
+    setText('');
+    setTodos(prevTodos => {
+      return [
+        { text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    });
+  } 
+
   return (
     <View style={styles.container}>
           <Header />
-      <Text style={styles.paragraph}>
-            Todolist is working!!
-      </Text>
+      <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+       <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+        </View>
+      </View>    
     </View>
   );
 }
@@ -25,10 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ecf0f1',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  content: {
+    padding: 40,
   },
 });
